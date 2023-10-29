61fab416fb83fb96365a07a4f8d7a190b8c00f34
Matt White <matt@matthewjwhite.co.uk>
2023-10-29 20:38:40 +0000

    add tags to resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,6 +33,7 @@ locals {
 resource "azurerm_resource_group" "dnszones" {
   name     = "rg-whitefam-dnszones"
   location = "UK South"
+  tags     = local.tags
   lifecycle {
     prevent_destroy = true
   }
@@ -41,6 +42,7 @@ resource "azurerm_resource_group" "dnszones" {
 resource "azurerm_resource_group" "cdnprofiles" {
   name     = "rg-whitefam-cdnprofiles"
   location = "UK South"
+  tags     = local.tags
   lifecycle {
     prevent_destroy = true
   }
@@ -51,4 +53,5 @@ resource "azurerm_cdn_profile" "cdm-mta-sts" {
   location            = "global"
   resource_group_name = azurerm_resource_group.cdnprofiles.name
   sku                 = "Standard_Microsoft"
+  tags                = local.tags
 }
