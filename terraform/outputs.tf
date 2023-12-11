e65cdeae71e124e5d20694478d44eef839d1164d
Matt White <matt@matthewjwhite.co.uk>
2023-12-11 20:23:36 +0000

    add private zone output for cross resource

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,9 @@
-output "privatezone-keyvault" {
+output "privatezone-keyvault-name" {
   value = azurerm_private_dns_zone.keyvault.name
+}
+
+output "privatezone-keyvault-id" {
+  value = azurerm_private_dns_zone.keyvault.id
 }
 
 output "rg-dnszones" {
