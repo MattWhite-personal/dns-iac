86bcc614345c0fee0e8ce66d91c87cb276891c8b
Matt White <matt@matthewjwhite.co.uk>
2025-02-09 21:22:33 +0000

    update remote state to use OIDC

--- terraform/remote_state.tf
+++ terraform/remote_state.tf
@@ -2,9 +2,10 @@ data "terraform_remote_state" "web-server" {
   backend = "azurerm"
 
   config = {
+    resource_group_name  = "rg-whitefam-terraform"
     storage_account_name = "stwhitefamterraform"
     container_name       = "tfstate"
     key                  = "web-hosting.tfstate"
-
+    use_oidc             = true
   }
 }
