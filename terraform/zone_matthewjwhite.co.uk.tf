105315aef59a37419efb3d14c52b59948cf466ab
Matt White <matt@matthewjwhite.co.uk>
2025-10-19 21:44:50 +0100

    update matthewjwhite.co.uk to AFD - fixes #100

--- terraform/zone_matthewjwhite.co.uk.tf
+++ terraform/zone_matthewjwhite.co.uk.tf
@@ -227,17 +227,17 @@ module "mjw-records" {
 }
 
 module "mjw-mtasts" {
-  source                   = "./module/mtasts"
-  use-existing-cdn-profile = true
-  existing-cdn-profile     = azurerm_cdn_profile.cdn-mta-sts.name
-  cdn-resource-group       = azurerm_resource_group.cdnprofiles.name
-  dns-resource-group       = azurerm_resource_group.dnszones.name
-  stg-resource-group       = "RG-WhiteFam-UKS"
-  mx-records               = ["matthewjwhite-co-uk.mail.protection.outlook.com"]
-  domain-name              = azurerm_dns_zone.matthewjwhite-co-uk.name
-  depends_on               = [azurerm_resource_group.cdnprofiles, azurerm_resource_group.dnszones]
-  reporting-email          = "tls-reports@matthewjwhite.co.uk"
-  resource-prefix          = "mjw"
-  tags                     = local.tags
-  permitted-ips            = local.permitted_ips
+  source                  = "./module/mtasts-v2"
+  use-existing-front-door = true
+  existing-front-door     = data.terraform_remote_state.web-server.outputs.whitefam-afd
+  afd-resource-group      = data.terraform_remote_state.web-server.outputs.afd-resource-group
+  dns-resource-group      = azurerm_resource_group.dnszones.name
+  stg-resource-group      = "RG-WhiteFam-UKS"
+  mx-records              = ["matthewjwhite-co-uk.mail.protection.outlook.com"]
+  domain-name             = azurerm_dns_zone.matthewjwhite-co-uk.name
+  depends_on              = [azurerm_resource_group.cdnprofiles, azurerm_resource_group.dnszones]
+  reporting-email         = "tls-reports@matthewjwhite.co.uk"
+  resource-prefix         = "mjw"
+  tags                    = local.tags
+  runner-ip               = var.runner-ip
 }
